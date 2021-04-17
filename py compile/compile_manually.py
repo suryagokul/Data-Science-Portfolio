f23152b9835735d5f5cd265684bdc5988d403e6d
Surya <41636357+suryagokul@users.noreply.github.com>
2021-04-17 12:37:54 +0530

    Add files via upload

---
+++ py compile/compile_manually.py
@@ -0,0 +1,7 @@
+import py_compile
+
+py_compile.compile('matrix_mul.py')
+
+# the above line compiles the python file and creates __pycache__ folder which has .pyc file saved as <<file_name>>.cpython-<<python_version>>.pyc
+
+ 
