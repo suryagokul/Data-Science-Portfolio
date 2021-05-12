42c26cc6377efbd9f212a259d779ef151a46a92c
Surya <41636357+suryagokul@users.noreply.github.com>
2021-05-12 10:21:35 +0530

    Update pattern_matching.py

--- regex/pattern_matching.py
+++ regex/pattern_matching.py
@@ -1,1 +1,16 @@
+import re
 
+# regex object
+pattern = re.compile("@gmail.com")
+
+print(pattern)
+
+# returns all matches as an iterator
+matches = pattern.finditer("hello hai my name is xyz and mail is abcde@gmail.com, support@gmail.com")
+
+print(matches)
+
+# or we can also do without compile as re.finditer('@gmail..',"hello hai....")
+
+for match in matches:
+    print("pattern is ", match.group(),match.start(), match.end())
